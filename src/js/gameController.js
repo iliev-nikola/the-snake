156ba7d66d53edabae7c78757640ba53cb3330a7
Nikola Iliev <nikola.r.iliev.91@gmail.com>
2021-04-26 20:35:27 +0300

    Fixing an issue for taking delay when changing snake position

--- src/js/gameController.js
+++ src/js/gameController.js
@@ -1,4 +1,4 @@
-// setup the size of the game over screen
+// Setup the size of the game over screen
 GAME_OVER_SCREEN.style.width = settings.width * 10 + 'px';
 GAME_OVER_SCREEN.style.height = settings.height * 10 + 'px';
 
@@ -107,19 +107,19 @@ document.body.addEventListener('keydown', (e) => {
         return makeInitialBox();
     }
 
-    if (settings.isGameOver) {
-        return;
-    }
-
     if (e.key === 'ArrowUp') {
         if (lastDirection === 'up' || lastDirection === 'down') {
             return;
         }
         clearInterval(timer);
-        timer = setInterval(() => {
-            move('up');
+        move('up');
+        if (!settings.isGameOver) {
             render();
-        }, settings.speed);
+            timer = setInterval(() => {
+                move('up');
+                render();
+            }, settings.speed);
+        }
     } else if (e.key === 'ArrowRight') {
         if (lastDirection === 'right' || lastDirection === 'left') {
             return;
@@ -127,28 +127,40 @@ document.body.addEventListener('keydown', (e) => {
             snake = snake.reverse();
         }
         clearInterval(timer);
-        timer = setInterval(() => {
-            move('right');
+        move('right');
+        if (!settings.isGameOver) {
             render();
-        }, settings.speed);
+            timer = setInterval(() => {
+                move('right');
+                render();
+            }, settings.speed);
+        }
     } else if (e.key === 'ArrowDown') {
         if (lastDirection === 'down' || lastDirection === 'up') {
             return;
         }
         clearInterval(timer);
-        timer = setInterval(() => {
-            move('down');
+        move('down');
+        if (!settings.isGameOver) {
             render();
-        }, settings.speed);
+            timer = setInterval(() => {
+                move('down');
+                render();
+            }, settings.speed);
+        }
     } else if (e.key === 'ArrowLeft') {
         if (lastDirection === 'left' || lastDirection === 'right') {
             return;
         }
         clearInterval(timer);
-        timer = setInterval(() => {
-            move('left');
+        move('left');
+        if (!settings.isGameOver) {
             render();
-        }, settings.speed);
+            timer = setInterval(() => {
+                move('left');
+                render();
+            }, settings.speed);
+        }
     }
 });
 
